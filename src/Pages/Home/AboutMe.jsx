b2449943f397858f8204c3a5f722cde281518aa7
VaidyaHarshal <harshal.vaidya300@gmail.com>
2023-09-05 19:31:07 -0400

    Adding the about me and portfolio section

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,28 @@
+export default function AboutMe() {
+    return (
+        <section id='AboutMe' className='about--section'>
+            <div className='about--section--img'>
+                <img src='./img/LinkedInCopy.jpg' alt='About Me' />
+            </div>
+            <div className='hero--section--content--box about--section--box'>
+                <div className='hero--section--content'>
+                    <p className='section--title'>About</p>
+                    <h1 className='skills--section--heading'>About Me</h1>
+                    <p className='hero--section--description'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quod alias ratione molestiae. Fugit velit ipsum corporis
+                        facere quia repellat quaerat eius voluptate molestiae
+                        incidunt, qui consequuntur voluptatibus debitis
+                        necessitatibus tempore!
+                    </p>
+                    <p className='hero--section--description'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quod alias ratione molestiae. Fugit velit ipsum corporis
+                        facere quia repellat quaerat eius voluptate molestiae
+                        incidunt.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
