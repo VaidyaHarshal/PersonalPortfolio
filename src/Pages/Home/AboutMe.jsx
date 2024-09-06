99a9d34b47ed8af1300ffdc161be4cac6cf30bc4
Harshal Vaidya <harshal.vaidya300@gmail.com>
2024-09-06 17:35:10 -0400

    css changes

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -5,33 +5,29 @@ export default function AboutMe() {
         <img src="./img/LinkedInCopy2.jpg" alt="About Me" />
       </div>
       <div className="hero--section--content--box about--section--box">
-        <div className="hero--section--content">
-          <p className="section--title">About</p>
-          <h1 className="skills--section--heading">About Me</h1>
-          <p className="hero--section--description">
-            Hello, I'm Harshal Vaidya, a versatile software engineer with a
-            passion for crafting innovative solutions. My journey is marked by
-            academic excellence, professional achievements, and a commitment to
-            staying at the forefront of technology.
-          </p>
-          <p className="hero--section--description">
-            I hold a Master's in Computer Science from Stevens Institute of
-            Technology, where I maintained a GPA of 3.9/4. My professional
-            journey includes a tenure as a Software Engineer at ICICI
-            Securities. There, I optimized performance, achieved significant
-            investment growth, and developed critical monitoring systems.
-          </p>
-          <p className="hero--section--description">
-            I've undertaken challenging projects that showcase my
-            problem-solving abilities. Notable examples include 'House of
-            Books,' demonstrating my proficiency in React, Redux, and automated
-            hosting, and 'My Parking Assistant' highlighting my web development
-            skills with Node.js, MongoDB. Beyond my technical expertise, I've
-            contributed to the academic community as a Graduate Peer Leader at
-            Stevens Institute, providing mentorship and support to fellow
-            students.
-          </p>
-        </div>
+        <h1 className="skills--section--heading">About Me</h1>
+        <p className="hero--section--description">
+          Hello, I'm Harshal Vaidya, a versatile software engineer with a
+          passion for crafting innovative solutions. My journey is marked by
+          academic excellence, professional achievements, and a commitment to
+          staying at the forefront of technology.
+        </p>
+        <p className="hero--section--description">
+          I hold a Master's in Computer Science from Stevens Institute of
+          Technology, where I maintained a GPA of 3.9/4. My professional journey
+          includes a tenure as a Software Engineer at ICICI Securities. There, I
+          optimized performance, achieved significant investment growth, and
+          developed critical monitoring systems.
+        </p>
+        <p className="hero--section--description">
+          I've undertaken challenging projects that showcase my problem-solving
+          abilities. Notable examples include 'House of Books,' demonstrating my
+          proficiency in React, Redux, and automated hosting, and 'My Parking
+          Assistant' highlighting my web development skills with Node.js,
+          MongoDB. Beyond my technical expertise, I've contributed to the
+          academic community as a Graduate Peer Leader at Stevens Institute,
+          providing mentorship and support to fellow students.
+        </p>
       </div>
     </section>
   );
