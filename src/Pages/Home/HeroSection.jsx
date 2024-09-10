c2dd57c4adda1adf5ae0b23ce6d51acc9b79a863
Harshal Vaidya <harshal.vaidya300@gmail.com>
2024-09-09 23:31:39 -0400

    modified years of experience in hero section

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,45 +1,41 @@
-import CV from '../../../assets/HarshalVaidya_Resume.pdf';
+import CV from "../../../assets/HarshalVaidya_Resume.pdf";
 
 export default function HeroSection() {
-    const linkedInUrl = 'https://www.linkedin.com/in/harshal-vaidya/';
+  const linkedInUrl = "https://www.linkedin.com/in/harshal-vaidya/";
 
-    const handleClick = () => {
-        window.open(linkedInUrl, '_blank');
-    };
-    return (
-        <section id='heroSection' className='hero--section'>
-            <div className='hero--section--content--box'>
-                <div className='hero--section--content'>
-                    <p className='section--title'>Hey, I'm Harshal</p>
-                    <h1 className='hero--section--title'>
-                        <span className='hero--section--title--color'>
-                            Software
-                        </span>
-                        <br />
-                        Developer
-                    </h1>
-                    <p className='hero--section--description'>
-                        A dedicated and results-driven software engineer with
-                        three years of industry experience and a passion for
-                        learning and building.
-                        <br />
-                        Seeking opportunities with teams that prioritize
-                        collaboration and creative problem-solving in shaping
-                        impactful, growing products.
-                    </p>
-                </div>
-                <div className='hero--section--contact--button'>
-                    <button className='btn btn-primary' onClick={handleClick}>
-                        Get in Touch
-                    </button>
-                    <a download='' href={CV} className='btn btn-primary'>
-                        Download CV
-                    </a>
-                </div>
-            </div>
-            <div className='hero--section--img'>
-                <img src='./img/LinkedInCopy.jpg' alt='Hero Section' />
-            </div>
-        </section>
-    );
+  const handleClick = () => {
+    window.open(linkedInUrl, "_blank");
+  };
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Harshal</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section--title--color">Software</span>
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section--description">
+            A dedicated and results-driven software engineer with four years of
+            industry experience and a passion for learning and building.
+            <br />
+            Seeking opportunities with teams that prioritize collaboration and
+            creative problem-solving in shaping impactful, growing products.
+          </p>
+        </div>
+        <div className="hero--section--contact--button">
+          <button className="btn btn-primary" onClick={handleClick}>
+            Get in Touch
+          </button>
+          <a download="" href={CV} className="btn btn-primary">
+            Download CV
+          </a>
+        </div>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/LinkedInCopy.jpg" alt="Hero Section" />
+      </div>
+    </section>
+  );
 }
