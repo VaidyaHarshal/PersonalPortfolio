99a957f4c63138997c7b2c3a895d4851df083b0d
Harshal Vaidya <harshal.vaidya300@gmail.com>
2025-01-22 22:00:19 -0500

    changed about me description

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -7,27 +7,43 @@ export default function AboutMe() {
       <div className="hero--section--content--box about--section--box">
         <h2 className="section--heading">About Me</h2>
         <p className="hero--section--description">
-          Hello, I'm Harshal Vaidya, a software engineer with a strong
-          background in web development. My journey is marked by academic
-          excellence, professional achievements, and a commitment to staying at
-          the forefront of technology.
+          Hello, I'm <strong>Harshal Vaidya</strong>, a software engineer with a
+          robust background in full-stack web development and a passion for
+          building impactful, user-centric solutions. My journey is marked by
+          academic excellence, professional achievements, and a dedication to
+          staying at the forefront of technology.
         </p>
         <p className="hero--section--description">
-          I hold a Master's degree in Computer Science from Stevens Institute of
-          Technology, where I maintained a GPA of 3.9/4. My professional journey
-          includes a tenure as a Software Engineer at ICICI Securities and Apex
-          Systems. There, I developed, optimized and tested frontend
-          functionalities providing efficient and engaging interfaces to users
-          enhancing user experience.
+          I hold a <strong>Master’s degree</strong> in{" "}
+          <strong>Computer Science</strong> from Stevens Institute of
+          Technology, graduating with a stellar GPA of <strong>3.9/4</strong>.
+          My professional experience spans dynamic roles, including my current
+          position as a Software Engineer at Radiance Technologies. There, I
+          have enhanced subscription-based systems, redesigned web applications
+          for improved user engagement, and optimized deployment pipelines using
+          <strong> AWS</strong>. Previously, at ICICI Securities, I scaled
+          financial systems, implemented subscription-based services, and
+          improved customer experience for millions of users.
         </p>
         <p className="hero--section--description">
-          I've undertaken challenging projects that showcase my problem-solving
-          abilities. Notable examples include 'House of Books,' demonstrating my
-          proficiency in React, Redux, and automated hosting, and 'My Parking
-          Assistant' highlighting my web development skills with Node.js,
-          MongoDB. Beyond my technical expertise, I've contributed to the
-          academic community as a Graduate Peer Leader at Stevens Institute,
-          providing mentorship and support to fellow students.
+          I have also undertaken challenging projects that showcase my
+          problem-solving abilities and technical expertise. Notable examples
+          include Crypto Trading View, a real-time cryptocurrency data
+          visualization platform built with <strong>React</strong> and{" "}
+          <strong>WebSockets</strong>, and House of Books, where I implemented
+          advanced caching techniques and <strong>Redux</strong> for seamless
+          state management. Additionally, during my tenure at Breathing.ai, I
+          championed accessibility by achieving <strong>WCAG</strong> compliance
+          and improving usability for disabled users.
+        </p>
+        <p className="hero--section--description">
+          Beyond my technical expertise, I have contributed to the community as
+          a Graduate Peer Leader at Stevens Institute, mentoring fellow students
+          and fostering collaborative learning. With a skill set that includes
+          <strong> React, Node.js, AWS,</strong> and <strong>Docker</strong>,
+          and experience in accessibility and performance optimization, I am
+          eager to continue creating innovative solutions that make a
+          difference.
         </p>
       </div>
     </section>
